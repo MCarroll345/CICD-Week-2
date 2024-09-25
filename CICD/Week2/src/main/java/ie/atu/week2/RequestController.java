37abebd74557eb649c708b6ac63fd86d8a8ff075
Mark Carroll <G00419267@atu.ie>
2024-09-25 15:24:29 +0100

    Exercise 4 working

--- CICD/Week2/src/main/java/ie/atu/week2/RequestController.java
+++ CICD/Week2/src/main/java/ie/atu/week2/RequestController.java
@@ -1,6 +1,7 @@
 package ie.atu.week2;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class RequestController {
 
     public String Hello(){
         return "Hello, world";
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name){
+        return "Name: " + name;
     }
 }
