5bdbbd762ebd1f00c044e2d49362034082a05162
Mark Carroll <G00419267@atu.ie>
2024-09-25 15:35:38 +0100

    Request Param working

--- CICD/Week2/src/main/java/ie/atu/week2/RequestController.java
+++ CICD/Week2/src/main/java/ie/atu/week2/RequestController.java
@@ -2,6 +2,7 @@ package ie.atu.week2;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,4 +18,11 @@ public class RequestController {
     public String greetByName(@PathVariable String name){
         return "Name: " + name;
     }
+
+    @GetMapping("/details")
+    public String nameOfTypeOfString(@RequestParam String name, int age){
+        return "Name: " + name + " Age: " + age;
+    }
+
+
 }
